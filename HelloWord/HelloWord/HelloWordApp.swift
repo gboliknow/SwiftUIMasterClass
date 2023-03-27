979122de4c82762f6c793ca06a9fca951d2c03bb
gbolahan <adekoyegbolahan001@gmail.com>
2023-03-27 15:36:07 +0100

    added hello world app

---
+++ HelloWord/HelloWord/HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  HelloWord
+//
+//  Created by Gbolahan on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
