9b3b32f7e532c38f3e50c4c08d07fb3ec5126901
gbolahan <adekoyegbolahan001@gmail.com>
2023-03-31 17:52:57 +0100

    added id for textTitle

--- RestartApp/RestartApp/Screens/OnboardingView.swift
+++ RestartApp/RestartApp/Screens/OnboardingView.swift
@@ -27,6 +27,7 @@ struct OnboardingView: View {
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
                         .transition(.opacity)
+                        .id(textTitle)
                     Text("""
     It's  not how much we give but how much love we put into giving.
     """)
