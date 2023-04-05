2ad890b651ad5106e8d61fd9cb65094feb53708f
gbolahan <adekoyegbolahan001@gmail.com>
2023-04-05 12:01:23 +0100

    finalize the pinch app

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var isAnimating : Bool = false
     @State private var imageScale : CGFloat = 1
     @State private var imageOffset : CGSize = CGSize(width: 0, height: 0)
+    @State private var isDrawerOpen : Bool = false
+    let pages : [Page] = pageDatas
+    @State private var pageIndex : Int = 1
+    
     // MARK: - FUNCTION
     func resetImageState(){
         withAnimation(.spring()){
@@ -18,13 +22,16 @@ struct ContentView: View {
             imageOffset = .zero
         }
     }
+    func currentPage() -> String {
+        return pages[pageIndex - 1].imageName
+    }
     var body: some View {
         NavigationView{
             ZStack{
                 
                 Color.clear
                 // MARK : - PAGE IMAGE
-                Image("magazine-front-cover")
+                Image(currentPage())
                     .resizable()
                     .aspectRatio( contentMode: .fit)
                     .cornerRadius(10)
@@ -133,10 +140,53 @@ struct ContentView: View {
                 .background(.ultraThinMaterial)
                 .cornerRadius(12)
                 .opacity(isAnimating ? 1 : 0)
+                
+          
            
             }
                 .padding(.bottom, 30),
                      alignment: .bottom
+                     
+            )
+            // MARK: - DRAWER
+            .overlay(
+                HStack(spacing: 12){
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" :  "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundColor(.secondary)
+                        .onTapGesture(perform :{
+                            withAnimation(.easeOut){
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    ForEach(pages) { page in
+                        Image(page.thumbnailName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width : 80)
+                            .cornerRadius(8)
+                            .shadow(radius: 4)
+                            .opacity(isDrawerOpen ? 1 : 0)
+                            .animation(.easeOut(duration: 0.5), value: isDrawerOpen)
+                            .onTapGesture(perform: {
+                                isAnimating = true
+                                pageIndex = page.id
+                            })
+                    }
+                    Spacer()
+                }
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 :0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment : .topTrailing
+                
             )
                      
         }.navigationViewStyle(.stack)
