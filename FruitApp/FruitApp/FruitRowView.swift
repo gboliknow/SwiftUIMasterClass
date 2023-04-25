3d9311ec97e64411a2322494c7a5f96f6f371a22
gbolahan <adekoyegbolahan001@gmail.com>
2023-04-25 14:31:38 +0100

    Added Home page to Fruit App

---
+++ FruitApp/FruitApp/FruitRowView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitRowView.swift
+//  FruitApp
+//
+//  Created by Gbolahan on 25/04/2023.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitRowView()
+    }
+}
