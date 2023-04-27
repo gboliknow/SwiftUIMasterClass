412327c8bbdfba8578c91e2746622d9a831557fb
gbolahan <adekoyegbolahan001@gmail.com>
2023-04-27 12:29:16 +0100

    added settings view

---
+++ FruitApp/FruitApp/SettingsLabelView.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsLabelView.swift
+//  FruitApp
+//
+//  Created by Gbolahan on 27/04/2023.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var labelText: String
+    var labelImage: String
+    var body: some View {
+        HStack {
+          Text(labelText.uppercased()).fontWeight(.bold)
+          Spacer()
+          Image(systemName: labelImage)
+        }
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
