857a59cc1a0b1b2ed7969dc6df1c7a1506087ead
gbolahan <adekoyegbolahan001@gmail.com>
2023-04-30 16:58:40 +0100

    :rocket: Finalize Fruit App

--- FruitApp/FruitApp/App/SettingsView.swift
+++ FruitApp/FruitApp/App/SettingsView.swift
@@ -34,6 +34,55 @@ struct SettingsView: View {
                       }
                     }
                     
+                    // MARK: - SECTION 2
+                    
+                    GroupBox(
+                      label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                      Divider().padding(.vertical, 4)
+                      
+                      Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                        .padding(.vertical, 8)
+                        .frame(minHeight: 60)
+                        .layoutPriority(1)
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                      
+                      Toggle(isOn: $isOnboarding) {
+                        if isOnboarding {
+                          Text("Restarted".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.green)
+                        } else {
+                          Text("Restart".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.secondary)
+                        }
+                      }
+                      .padding()
+                      .background(
+                        Color(UIColor.tertiarySystemBackground)
+                          .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                      )
+                    }
+                    
+                    
+                    
+                    // MARK: - SECTION 3
+                    
+                    GroupBox(
+                      label:
+                      SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ) {
+                      SettingsRowView(name: "Developer", content: "John / Jane")
+                      SettingsRowView(name: "Designer", content: "Robert Petras")
+                      SettingsRowView(name: "Compatibility", content: "iOS 16")
+                      SettingsRowView(name: "Website", linkLabel: "Credo Academy", linkDestination: "credo.academy")
+                      SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+                      SettingsRowView(name: "SwiftUI", content: "4")
+                      SettingsRowView(name: "Version", content: "1.5.0")
+                    }
+                    
                 }
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(
