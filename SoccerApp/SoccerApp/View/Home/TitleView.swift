5f1ec6cd68f3f6c890b94740ec8c1c8fc66bafe5
gbolahan <adekoyegbolahan001@gmail.com>
2023-07-05 14:37:01 +0100

    :art: Added categories  , helmets grid and brands grid

---
+++ SoccerApp/SoccerApp/View/Home/TitleView.swift
@@ -0,0 +1,36 @@
+//
+//  TitleView.swift
+//  SoccerApp
+//
+//  Created by Gbolahan on 05/07/2023.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    // MARK: - PREVIEW
+    
+    var title: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+          Text(title)
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+          
+          Spacer()
+        } //: HSTACK
+        .padding(.horizontal)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+      }
+    }
+
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView(title: "Helmet")
+          .previewLayout(.sizeThatFits)
+          .background(colorBackground)    }
+}
