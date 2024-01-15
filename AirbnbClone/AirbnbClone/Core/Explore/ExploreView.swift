80c1799b3bfc1ed8c87bfbb812cb12545110490c
gbolahan <adekoyegbolahan001@gmail.com>
2024-01-15 16:37:52 +0100

    :art: More on swift ui
    more with logic to dismiss and getting comfortable with swift ui

--- AirbnbClone/AirbnbClone/Core/Explore/ExploreView.swift
+++ AirbnbClone/AirbnbClone/Core/Explore/ExploreView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack{
-            ScrollView{
-                LazyVStack(spacing: 32){
-                    ForEach(0...10, id: \.self){ listing in
-                        
-                        ListingItemView().frame(height: 400)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                        
+            VStack {
+                SearchAndFilterBarView()
+                ScrollView{
+                    
+                    LazyVStack(spacing: 32){
+                        ForEach(0...10, id: \.self){ listing in
+                            
+                            
+                            NavigationLink(value: listing) {
+                                ListingItemView().frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
+                            
+                        }
                     }
+                    
                 }
-         
+                .navigationDestination(for: Int.self){ listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                    
+                }
             }
         }
     }
